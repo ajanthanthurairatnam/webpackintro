6a7a16c4791b132f5c416738cf37cd9ca0a2466c
ajanthan <ajanthan.kt@gmail.com>
2021-05-23 12:13:35 +1000

    Created new repo with file

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+import {wizzard} from './wizzard';
+import {barbarian} from './babarian';
 console.log("Ran from index js");
+console.log(wizzard);
+console.log(barbarian);
---
+++ wizzard.js
@@ -0,0 +1,1 @@
+export const wizzard="wizzard";
