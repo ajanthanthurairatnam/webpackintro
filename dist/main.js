b4cee86590df0648f31402f231b87a19a05b8a59
ajanthan <ajanthan.kt@gmail.com>
2021-05-23 13:49:01 +1000

    Babel configuration

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,116 @@
-(()=>{"use strict";console.log("Ran from index js"),console.log("wizzard"),console.log("barbarian")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/babarian.js":
+/*!*************************!*\
+  !*** ./src/babarian.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"babarian\": () => (/* binding */ babarian)\n/* harmony export */ });\nvar babarian = \"barbarian\";\n\n//# sourceURL=webpack://webpackintro/./src/babarian.js?");
+
+/***/ }),
+
+/***/ "./src/getclasses.js":
+/*!***************************!*\
+  !*** ./src/getclasses.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"getclasses\": () => (/* binding */ getclasses)\n/* harmony export */ });\n/* harmony import */ var _wizzard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./wizzard */ \"./src/wizzard.js\");\n/* harmony import */ var _babarian__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./babarian */ \"./src/babarian.js\");\n\n\nvar getclasses = function getclasses() {\n  console.log(_wizzard__WEBPACK_IMPORTED_MODULE_0__.wizzard);\n  console.log(_babarian__WEBPACK_IMPORTED_MODULE_1__.babarian);\n};\n\n//# sourceURL=webpack://webpackintro/./src/getclasses.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _getclasses__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./getclasses */ \"./src/getclasses.js\");\nfunction ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }\n\nfunction _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }\n\nfunction _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }\n\n\nconsole.log(\"Ran from index js\");\n(0,_getclasses__WEBPACK_IMPORTED_MODULE_0__.getclasses)();\nvar obj = {\n  a: 'alpha',\n  b: 'bravo'\n};\n\nvar newobj = _objectSpread(_objectSpread({}, obj), {}, {\n  c: 'charlie'\n});\n\nconsole.log(newobj);\n\n//# sourceURL=webpack://webpackintro/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/wizzard.js":
+/*!************************!*\
+  !*** ./src/wizzard.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"wizzard\": () => (/* binding */ wizzard)\n/* harmony export */ });\nvar wizzard = \"wizzard\";\n\n//# sourceURL=webpack://webpackintro/./src/wizzard.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 import {getclasses} from './getclasses';
 console.log("Ran from index js");
 getclasses();
+
+const obj = {a:'alpha', b:'bravo' }
+const newobj = { ...obj, c:'charlie'}
+console.log(newobj);
+
